bd56c4298637cfbb7bcddda4bd86b0a164b4154b
邓天 <17820457171@163.com>
2019-06-13 13:47:36 +0800

    Initial commit

---
+++ server/routes/trip.js
@@ -0,0 +1,145 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+
+var data = {
+    code: 200,
+    msg: 'success'
+}; //最后返回的json对象
+// 本地记录数据库中总出行里程
+var allDistanceData = {
+    allBus: 0,
+    allCycle: 0,
+    allDrive: 0,
+    allRun: 0,
+    allTaxi: 0,
+    allWalk: 0
+}
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'trip'
+});
+/* GET users listing. */
+// 历史列表接口
+router.get('/historyList', function (req, res, next) {
+    let tmpId = req.query.userId
+    //增删改查之后查询，并把查询的最终数据返回前端
+    pool.query(`SELECT * FROM trip_data where userId=${tmpId}`, function (err, results, fields) {
+        if (err) {
+            data.code = 500
+            data.msg = err
+        };
+        // 更改经纬度坐标数据类型
+        results.forEach((item) => {
+            if (item.trajectory) {
+                item.trajectory = JSON.parse(item.trajectory)
+            }
+            if (item.startCode) {
+                item.startCode = item.startCode.split(',')
+                item.endCode = item.endCode.split(',')
+            }
+        })
+        data.data = results.reverse()
+        res.statusCode = 200;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+})
+
+// 出行总公里数
+router.get('/allDistance', function (req, res, next) {
+    let tmpId = req.query.userId
+    //增删改查之后查询，并把查询的最终数据返回前端
+    pool.query(`SELECT * FROM total_data where userId=${tmpId}`, function (err, results, fields) {
+        if (err) {
+            data.code = 500
+            data.msg = err
+        };
+        // 更改经纬度坐标数据类型
+        data.data = results[0]
+        allDistanceData = results[0]
+        console.log(allDistanceData)
+        res.statusCode = 200;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+})
+// 插入出行数据
+router.get('/addTrip', function (req, res, next) {
+    let query = req.query
+    if (query.tripType === '徒步出行') {
+        allDistanceData.allWalk = (Number(allDistanceData.allWalk) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allWalk', allDistanceData.allWalk, query.userId)
+    } else if (query.tripType === '跑步出行') {
+        allDistanceData.allRun = (Number(allDistanceData.allRun) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allRun', allDistanceData.allRun, query.userId)
+    } else if (query.tripType === '骑车出行') {
+        allDistanceData.allCycle = (Number(allDistanceData.allCycle) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allCycle', allDistanceData.allCycle, query.userId)
+    } else if (query.tripType === '自驾出行') {
+        allDistanceData.allDrive = (Number(allDistanceData.allDrive) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allDrive', allDistanceData.allDrive, query.userId)
+    }
+    //增删改查之后查询，并把查询的最终数据返回前端
+    pool.query(`INSERT INTO trip_data ( userId, type,tripType,distance,date,time,trajectory,Calorie,speed,mark) VALUES ('${query.userId}','${query.type}','${query.tripType}','${query.distance}','${query.date}','${query.time}','${query.trajectory}','${query.Calorie}','${query.speed}','${query.mark}')`, function (err, results, fields) {
+        if (err) {
+            data.code = 500
+            data.msg = err
+        } else {
+            data.msg = '成功'
+            data.code = 200
+        }
+        // 更改经纬度坐标数据类型
+        res.statusCode = 200;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+})
+
+// 插入交通数据
+router.get('/addTraffic', function (req, res, next) {
+    let query = req.query
+    if (query.tripType === '步行') {
+        allDistanceData.allWalk = (Number(allDistanceData.allWalk) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allWalk', allDistanceData.allWalk, query.userId)
+    } else if (query.tripType === '出租车') {
+        allDistanceData.allTaxi = (Number(allDistanceData.allTaxi) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allTaxi', allDistanceData.allTaxi, query.userId)
+    } else if (query.tripType === '单车/电车') {
+        allDistanceData.allCycle = (Number(allDistanceData.allCycle) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allCycle', allDistanceData.allCycle, query.userId)
+    } else if (query.tripType === '公交/地铁') {
+        allDistanceData.allBus = (Number(allDistanceData.allBus) + Number(query.distance)).toFixed(2)
+        allDistanceUpdate('allBus', allDistanceData.allBus, query.userId)
+    }
+    //增删改查之后查询，并把查询的最终数据返回前端
+    pool.query(`INSERT INTO trip_data ( userId, type, tripType, distance, date, time, price, startPlace, endPlace, startCode, endCode, mark) VALUES ('${query.userId}','${query.type}','${query.tripType}','${query.distance}','${query.date}','${query.time}','${query.price}','${query.startPlace}','${query.endPlace}','${query.startCode}','${query.endCode}','${query.mark}')`, function (err, results, fields) {
+        if (err) {
+            data.code = 500
+            data.msg = err
+        } else {
+            data.msg = '成功'
+            data.code = 200
+        }
+        // 更改经纬度坐标数据类型
+        res.statusCode = 200;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+})
+
+// 更新里程
+function allDistanceUpdate(key, value, userId) {
+    console.log(`${key}出行里程更新`)
+    console.log(allDistanceData)
+    data.data = allDistanceData
+    pool.query(`UPDATE total_data SET ${key}=${value} where userId=${userId}`, function (err, results, fields) {})
+}
+module.exports = router;
---
+++ server/routes/user.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+
+var data = {
+  code: 200,
+  msg: 'success'
+}; //最后返回的json对象
+
+var pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'trip'
+});
+router.post('/', function (req, res, next) {
+  let userName = req.body.userName
+  let passWord = req.body.passWord
+  //增删改查之后查询，并把查询的最终数据返回前端
+  pool.query(`SELECT * FROM user where userName=${userName}`, function (err, results, fields) {
+    if (err) {
+      data.code = 500
+      data.msg = err
+    };
+    if (results[0].passWord === passWord) {
+      selectUser(results[0].userId)
+    } else {
+      data.code = 400
+      data.msg = '账号或密码输入有误'
+      res.statusCode = 200;
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader('Content-Type', 'application/json');
+      res.json(data);
+    }
+  });
+
+  function selectUser(id) {
+    pool.query(`SELECT * FROM user_info where userId=${id}`, function (err, results, fields) {
+      if (err) {
+        data.code = 500
+        data.msg = err
+      };
+      data.code = 200
+      data.msg = 'sucess'
+      data.data = results[0]
+      res.statusCode = 200;
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader('Content-Type', 'application/json');
+      res.json(data);
+    })
+  }
+})
+// 获取最近一周
+router.get('/tripTrend', function (req, res, next) {
+  let tmpId = req.query.userId
+  //增删改查之后查询，并把查询的最终数据返回前端
+
+  pool.query(`SELECT * FROM trip_data where userId=${tmpId} and DATE_SUB(CURDATE(), INTERVAL 6 DAY) <= date(date)`, function (err, results, fields) {
+    if (err) {
+      data.code = 500
+      data.msg = err
+    };
+    // 更改经纬度坐标数据类型
+    results.forEach((item) => {
+      if (item.trajectory) {
+        item.trajectory = JSON.parse(item.trajectory)
+      }
+      if (item.startCode) {
+        item.startCode = item.startCode.split(',')
+        item.endCode = item.endCode.split(',')
+      }
+    })
+    data.data = results.reverse()
+    res.statusCode = 200;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data);
+  });
+})
+module.exports = router;
